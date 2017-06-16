9d2f1850937b38d667474a854bc497ccf121be51
catcattac <catwong.nyc@gmail.com>
2017-06-16 00:07:40 -0400

    added search area with border

--- app/src/main/java/catwong/c4q/nyc/lastbottle/MainActivity.java
+++ app/src/main/java/catwong/c4q/nyc/lastbottle/MainActivity.java
@@ -1,13 +1,55 @@
 package catwong.c4q.nyc.lastbottle;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText editSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editSearch = (EditText) findViewById(R.id.editSearch);
+        editSearch.setBackgroundResource(R.drawable.search_border);
+        ShapeDrawable shape = new ShapeDrawable(new RectShape());
+        shape.getPaint().setColor(Color.BLACK);
+        shape.getPaint().setStyle(Paint.Style.STROKE);
+        shape.getPaint().setStrokeWidth(3);
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void handleMenuSearch() {
     }
 }
